c312505e180e7fb00242e13848a51134a0b217f7
Beixuan Yang <yangbeixuan@gmail.com>
2025-03-02 20:57:46 -0500

    add .dll to gitignore

--- obj/Debug/net9.0/MyBlazerWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyBlazerWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazerWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc6c7494892d466d8d1fb76e68793efb1595caec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e094f947174b5d8af9615a2fa5ebcbc47c6e1ab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazerWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazerWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
